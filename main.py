a5f710dda24604d4fe98f8cf43dd4a005a70ebe9
Vitalii Hryhorchuk <gvit84@gmail.com>
2023-01-04 15:42:40 +0200

    second commit main_function2

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 #Функція, яка приймає довільну кількість словників, збирає їх в один словник та повертає його
 main_dct = {}
 def main_function(**kwargs):
+    main_dct.update(**kwargs)
+    return main_dct
+
+
+main_function(brand='Volvo', model='XC90', year=2022)
+main_function(sedan='Volkswagen', Coupe='Porsche')
+main_function(January='spring', June='summer')
+main_function(name='Vitalii', age=38, eyes_color='green')
