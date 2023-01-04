39e029df81397a2498d921e245daa4c8db28b642
Vitalii Hryhorchuk <gvit84@gmail.com>
2023-01-04 16:11:43 +0200

    fifth commit test_function1

--- main.py
+++ main.py
@@ -10,3 +10,9 @@ main_function(sedan='Volkswagen', Coupe='Porsche')
 main_function(January='spring', June='summer')
 main_function(name='Vitalii', age=38, eyes_color='green')
 print(main_dct)
+
+
+#Функція, яка приймає номер місяця та повертає рядок з назвою пори року, до якої цей місяць відноситься
+def year_seasons(a):
+    dct = {1: "winter", 2: "winter", 3: "spring", 4: "spring", 5:"spring", 6: "summer",
+           7: "summer", 8: "summer", 9: "autumn", 10: "autumn", 11: "autumn", 12: "winter"}
